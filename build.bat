9dcf1e28cbd558674f5a11bea76a70a345d364f4
ricohoho <ricohoho@gmail.com>
2022-11-19 22:45:51 +0100

    test commit Built.bat

---
+++ build.bat
@@ -0,0 +1,3 @@
+ng build --deploy-url /ricofilmA/ --configuration=production
+REM copier / coller du sous répertoire /dist/ dans  : davic.mkdh.fr : /var/www/html/ricofilmA
+REM Test http://davic.mkdh.fr/ricofilmA/ 

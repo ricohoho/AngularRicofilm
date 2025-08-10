eb0442be65001ab87bc281cfeebab1c4cd17caa1
ricohoho <ricohoho@gmail.com>
2025-08-11 00:40:12 +0200

    Cors 5 me

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 //const AUTH_API = 'http://localhost:3000/api/auth/';
 //const AUTH_API = 'http://localhost:4200/api/auth/';
 const AUTH_API = '/api/auth/';
+const REST_HOST = environment.REST_HOST;
+
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -44,7 +47,7 @@ export class AuthService {
     console.log('URL:'+AUTH_API + 'signout')
     //return this.http.get(AUTH_API + 'signout', httpOptions);
     //return this.http.get(AUTH_API + 'dcnx', httpOptions);
-    return this.http.get(AUTH_API + 'dcnx');
+    return this.http.get(REST_HOST+AUTH_API + 'dcnx');
     //return this.http.get('http://localhost:4200/api/test/all');
 
 

ca2220c63c7ccf8154ec6e0277c812b53ced6510
ricohoho <ricohoho@gmail.com>
2025-11-22 18:02:45 +0100

    environement.cloud

--- src/environments/environment.cloud-render.ts
+++ src/environments/environment.cloud-render.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   //si frnt et back eme host laisser a vide sion indiquer l'url du back
   REST_HOST: 'https://ricofilm-web.onrender.com/',
-  REST_API_FILM_SERVER :'films/list',
-  REST_API_FILM_SERVER_SELECT :'films/listselect',
-  REST_API_FILM_MENU_IMAGE :'films/listmenufilmimage',
-  REST_API_USER_SERVER_SELECT : '/api/user/list',
-  REST_API : 'api',
-  REST_API_REQUEST_SERVER :'request',
-  PATH_IMAGE:'../../assets/images/',
-  URL_HOME:''
+  REST_API_FILM_SERVER: 'films/list',
+  REST_API_FILM_SERVER_SELECT: 'films/listselect',
+  REST_API_FILM_MENU_IMAGE: 'films/listmenufilmimage',
+  REST_API_USER_SERVER_SELECT: 'api/user/list',
+  REST_API: 'api',
+  REST_API_REQUEST_SERVER: 'request',
+  PATH_IMAGE: '../../assets/images/',
+  URL_HOME: ''
 };
